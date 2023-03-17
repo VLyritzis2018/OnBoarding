ee02b34aecdb58a1fd3ea00c358a1535d7e467a8
d76g <basharalshaibani@gmail.com>
2023-03-17 13:57:05 +0100

    Adding Google Analysis tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,15 @@
         {{-- CSS Animation --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3RN3N63DP5"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3RN3N63DP5');
+        </script>
         <style>
             body {
                 font-family: 'Comfortaa', cursive;
